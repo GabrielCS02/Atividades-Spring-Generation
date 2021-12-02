ef1a52adb0969a2b66359d9e587ea98cbb208015
Gabriel Costa Silva <93771459+GabrielCS02@users.noreply.github.com>
2021-12-02 15:07:32 -0300

    Adicionando Post, Put e Delete

--- meuBlog/src/main/java/org/generation/meuBlog/controller/PostagemController.java
+++ meuBlog/src/main/java/org/generation/meuBlog/controller/PostagemController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.generation.meuBlog.model.Postagem;
 import org.generation.meuBlog.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,4 +44,20 @@ public class PostagemController {
 	  public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
 		  return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 		}
+	  // Adiciona uma postagem
+	  @PostMapping
+	  public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+		  return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	  //altera uma postagem
+	  }
+	  @PutMapping
+	  public ResponseEntity<Postagem> put (@RequestBody Postagem postagem){
+		  return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+	  }
+	  
+	  @DeleteMapping("/{id}")
+	  public void delete(@PathVariable long id) {
+		  postagemRepository.deleteById(id);
+	  }
 }
+
